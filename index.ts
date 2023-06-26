000796128f15a0cb5e7b7ea90d60aa1ddf044e11
Alessandro1918 <alessandro1918@hotmail.com>
2023-06-26 17:27:35 -0300

    build: added 'cors' package

--- index.ts
+++ index.ts
@@ -2,12 +2,17 @@ import express from "express"
 import { createServer } from "http"
 import { Server, Socket } from "socket.io"
 import { v4 as uuidv4 } from "uuid"
+import cors from "cors"
 import "dotenv/config"
 
 const PORT = process.env.PORT || 4000
 const URL_FRONT = process.env.URL_FRONT || "http://localhost:3000"
 
 const app = express()
+
+app.use(cors({
+  origin: URL_FRONT
+}))
 
 app.get("/", (req, res) => {
   res.send(`<h1>Hello, World!</h1>`)
